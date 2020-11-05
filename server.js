f62c19bea76594af160b3d7fbfa254d9eef8b683
Aveen Pattni <aveenpattni@gmail.com>
2020-11-05 10:43:27 -0500

    add cors

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+
 
 // Define Constants
 const PORT = process.env.PORT || 8000;
 
 // Express App set up
 const app = express();
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'client/build')));
